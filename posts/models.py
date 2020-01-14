7722eb8bd68e312822fd399fb69426123c17d71c
Cristhian Forero <cristhian.forero.dominguez@gmail.com>
2020-01-13 22:55:21 -0500

    Se agrega modelo User

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,16 @@
+""" Posts Models. """
+# Django
 from django.db import models
 
-# Create your models here.
+
+class User(models.Model):
+    """User Model"""
+
+    email = models.EmailField(unique=True)
+    password = models.CharField(max_length=100)
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    bio = models.TextField(blank=True)
+    birth_date = models.DateField(blank=True, null=True)
+    created = models.DateTimeField(auto_now_add=True)
+    modified = models.DateTimeField(auto_now=True)
