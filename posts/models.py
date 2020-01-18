311751c15ac999520704e28f5df185ce4cc37c60
Cristhian Forero <cristhian.forero.dominguez@gmail.com>
2020-01-18 12:15:28 -0500

    Se agregan campos al modelo

--- posts/models.py
+++ posts/models.py
@@ -10,7 +10,13 @@ class User(models.Model):
     password = models.CharField(max_length=100)
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
+    country = models.CharField(max_length=50, blank=True)
+    city = models.CharField(max_length=50, blank=True)
     bio = models.TextField(blank=True)
     birth_date = models.DateField(blank=True, null=True)
+    is_admin = models.BooleanField(default=False)
     created = models.DateTimeField(auto_now_add=True)
     modified = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.email
